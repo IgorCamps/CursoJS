2b3a8da3c234fb15abea3cf49adb5851146355d4
IgorCamps <igoruser07@gmail.com>
2024-01-30 23:56:44 -0300

    Vetor, for e for in

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,8 +1,12 @@
 let num = [5,8,2,9,3]
-num.sort()
 num.push(1)
+num.sort()
 console.log(num)
 console.log(`O vetor tem ${num.length} posições`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
-
-
+console.log(`O primeiro valor do vetor [e ${num[0]}]`)
+let pos = num.indexOf(2)
+if(pos == -1)
+    console.log(`O valor não foi encontrado`)
+else
+    console.log(`O valor 8 está na posição ${pos}`)
