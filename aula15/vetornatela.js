42cf26ccaa156b168a34790bcb4afe534517217d
IgorCamps <igoruser07@gmail.com>
2024-02-02 12:11:39 -0300

    Execs

--- aula15/vetornatela.js
+++ aula15/vetornatela.js
@@ -1,12 +1,28 @@
-let valores = [8, 1, 7, 4, 2 , 9]
 /*
+
+let valores = [8, 1, 7, 4, 2 , 9]
 for (let pos = 0; pos < valores.length; pos++) {
    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)    
 }
-*/
-
 
 for (let pos in valores) {
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }
+*/
 
+let pessoa = [
+    {
+        id: 10,
+        "nome": "Igor"
+    },
+    {
+        id: 19,
+        "nome" : "Campos"   
+    },
+    {
+        id: 22,
+        "nome": "Anakin"
+    }
+    ]
+
+console.log(pessoa.length)
---
+++ aula17/objeto01.js
@@ -0,0 +1,20 @@
+// let amigo = {nome: "José",
+// sexo: 'M',
+// peso: 85.4,
+// engordar(p = 0){
+//     console.log('Engordou')
+//     this.peso += p
+// }}
+
+// amigo.engordar(2)
+// console.log(`${amigo.nome} pesa ${amigo.peso}Kg`)
+
+
+
+let amigos = [
+        {nome: "Igor", idade: 21},
+        { nome: "Pedro", idade: 20}
+    ]
+
+
+console.log(amigos)
